3078c61d329ea1306c4dc9e9c39e04f67791b26b
Rahul <rahuldavu23@gmail.com>
2025-03-31 15:45:05 -0400

    Did the second one

--- src/functions.ts
+++ src/functions.ts
@@ -13,6 +13,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
+    let sum = 0;
+    if (first > 0) {
+        sum += first;
+    }
+    if (second > 0) {
+        sum += second;
+    }
+    if (third > 0) {
+        sum += third;
+    }
+    console.log(sum);
     return 0;
 }
 
