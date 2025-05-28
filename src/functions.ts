23624c8200d0c76025366325c6558df419a9e222
Rahul <rahuldavu23@gmail.com>
2025-05-27 21:44:12 -0400

    Refixed changes

--- src/functions.ts
+++ src/functions.ts
@@ -13,15 +13,9 @@ export function fahrenheitToCelsius(temperature: number): number {
  */
 export function add3(first: number, second: number, third: number): number {
     let sum = 0;
-    if (first > 0) {
-        sum += first;
-    }
-    if (second > 0) {
-        sum += second;
-    }
-    if (third > 0) {
-        sum += third;
-    }
+    if (first > 0) sum += first;
+    if (second > 0) sum += second;
+    if (third > 0) sum += third;
     return sum;
 }
 
